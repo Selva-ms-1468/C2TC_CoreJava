d543fbd46480ddca2f8127d9cfc1dae8330eee2b
Selva <Selva@Scarlett>
2022-05-31 15:23:53 +0530

    first comment

---
+++ Day 1/src/Alphabet.java
@@ -0,0 +1,16 @@
+
+public class Alphabet {
+
+	class main
+	{
+		public main(String[] args)
+	
+    {char a;
+	if(Character.isAlphabetic(a))
+		System.out.println(a+"is an alphabet");
+	else
+		System.out.println(a+"is not an alphabet");
+    }
+
+	}
+}
---
+++ Day 1/src/this_keyword.java
@@ -0,0 +1,20 @@
+
+public class this_keyword {
+
+	class Demo
+	{
+		void m()
+		{
+			System.out.println(this);
+		}
+		public static void main(String args[])
+		{
+			Demo obj = new Demo();
+			System.out.println(obj);
+			obj.m();
+		}
+	}
+	
+	
+	
+}
